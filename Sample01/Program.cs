7a20a060c48da1992d4cf4bbc989c3af65e2962a
Jiajia Duan <1160419752@qq.com>
2018-12-18 12:05:53 +0800

    测试git上传代码

---
+++ Sample01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+             {
+                 var env = hostingContext.HostingEnvironment;
+                 config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                               .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                               .AddEnvironmentVariables();
+             })
+             .UseStartup<Startup>();
+    }
+}
